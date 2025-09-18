d5adfe7fb046e3b9f460adf4db74bef227176d5e
Nilton Perim Neto <niltonperimneto@gmail.com>
2025-09-18 15:41:14 -0300

    Fixed the JS code for DataTables

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,16 +1,12 @@
-﻿import DataTable from 'datatables.net-dt';
- 
-let table = new DataTable('#tabela-contatos');
-
-// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-//i'm keeping it in mind that THIS bit of code might break and fuck everything up
 $(document).ready(function () {
-    $('#tabela-contatos').DataTable();
+    $('#tabela-contatos').DataTable({
+        
+    });
 });
+
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
-})
-//alright so the problem:
-//the fuckwads who made evolua forgot to update 
+});
