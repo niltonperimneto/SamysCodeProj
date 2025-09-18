3689e363fa9346eed2bddebea8c334d6b6e3615d
YaGalRazielle <149109971+YaGalRazielle@users.noreply.github.com>
2025-09-18 15:30:44 -0300

    wawawawawawawawwawaawaw

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,16 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿import DataTable from 'datatables.net-dt';
+ 
+let table = new DataTable('#tabela-contatos');
+
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-
+//i'm keeping it in mind that THIS bit of code might break and fuck everything up
+$(document).ready(function () {
+    $('#tabela-contatos').DataTable();
+});
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
 })
+//alright so the problem:
+//the fuckwads who made evolua forgot to update 
